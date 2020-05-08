26854ce55213a580bf8cf0095682833c76e39675
sebastienlanglois <sebastien.langlois@polymtl.ca>
2020-05-08 15:10:23 -0400

    changed module name from dss to hec

--- laminage/__init__.py
+++ laminage/__init__.py
@@ -1,4 +1,4 @@
-from .dss.csvtodss import csv_to_dss
+from .hec.csvtodss import csv_to_dss
 
 # A hardcoded __all__ variable is necessary to appease
 # `mypy --strict` running in projects that import xarray.
