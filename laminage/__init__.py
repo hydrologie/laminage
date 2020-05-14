49d746da6ca8113a94549c2ec76b2b807c89399b
sebastienlanglois <sebastien.langlois@polymtl.ca>
2020-05-14 13:12:28 -0400

    new functions to init

--- laminage/__init__.py
+++ laminage/__init__.py
@@ -1,9 +1,9 @@
-from .hec.csvtodss import csv_to_dss
 from .hec.alternatives import CreationAlternative
+from .hec.base import BaseManager
 # A hardcoded __all__ variable is necessary to appease
 # `mypy --strict` running in projects that import xarray.
 __all__ = (
     # Top-level functions
-    "csv_to_dss",
-    "CreationAlternative"
+    "CreationAlternative",
+    "BaseManager"
 )
